8a8529b4e900f35075574a9d22fd3110771ae274
Ph3telecom <144226688+Ph3telecom@users.noreply.github.com>
2023-09-06 16:07:41 +0100

    Add files via upload

---
+++ data/DataCentreCLS_5.js
@@ -0,0 +1,26 @@
+var json_DataCentreCLS_5 = {
+"type": "FeatureCollection",
+"name": "DataCentreCLS_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Data Cente": "WACS", "Latitude": "6.425102778", "Longitude": "3.639877778" }, "geometry": { "type": "Point", "coordinates": [ 3.639877778, 6.425102778 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Mainone", "Latitude": "6.449313889", "Longitude": "3.588713889" }, "geometry": { "type": "Point", "coordinates": [ 3.588713889, 6.449313889 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Medallion", "Latitude": "6.426647222", "Longitude": "3.417422222" }, "geometry": { "type": "Point", "coordinates": [ 3.417422222, 6.426647222 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Rack Center", "Latitude": "6.597513889", "Longitude": "3.3735" }, "geometry": { "type": "Point", "coordinates": [ 3.3735, 6.597513889 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "MDXi", "Latitude": "6.450491667", "Longitude": "3.592119444" }, "geometry": { "type": "Point", "coordinates": [ 3.592119444, 6.450491667 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "21st Centurey", "Latitude": "6.448336111", "Longitude": "3.478275" }, "geometry": { "type": "Point", "coordinates": [ 3.478275, 6.448336111 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "MTN", "Latitude": "6.5864", "Longitude": "3.377066667" }, "geometry": { "type": "Point", "coordinates": [ 3.377066667, 6.5864 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "GLO", "Latitude": "6.433262", "Longitude": "3.5231051" }, "geometry": { "type": "Point", "coordinates": [ 3.5231051, 6.433262 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Open Access Data Centre", "Latitude": "6.424126586537633", "Longitude": "3.4978866967689046" }, "geometry": { "type": "Point", "coordinates": [ 3.497886696768905, 6.424126586537633 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "IS Nigeria", "Latitude": "6.4344381518685525", "Longitude": "3.4392727497873787" }, "geometry": { "type": "Point", "coordinates": [ 3.439272749787379, 6.434438151868553 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Meta", "Latitude": "6.4255335044811535", "Longitude": "3.641656320693283" }, "geometry": { "type": "Point", "coordinates": [ 3.641656320693283, 6.425533504481153 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Meta- Kwa Ibom", "Latitude": "4.543679960810799", "Longitude": "8.004124461801439" }, "geometry": { "type": "Point", "coordinates": [ 8.004124461801439, 4.543679960810799 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Kasi", "Latitude": "6.42738931175149", "Longitude": "3.5099665286497483" }, "geometry": { "type": "Point", "coordinates": [ 3.509966528649748, 6.42738931175149 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Kasi-Eket", "Latitude": "4.628259674330591", "Longitude": "7.93498867957393" }, "geometry": { "type": "Point", "coordinates": [ 7.93498867957393, 4.628259674330591 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Galaxy Backbone", "Latitude": "9.065011453825765", "Longitude": "7.494184847078044" }, "geometry": { "type": "Point", "coordinates": [ 7.494184847078044, 9.065011453825765 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Africa Data Centre", "Latitude": "6.412965087288926", "Longitude": "3.4074954846925203" }, "geometry": { "type": "Point", "coordinates": [ 3.40749548469252, 6.412965087288926 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Medallion", "Latitude": "9.070246516495853", "Longitude": "7.480000437740641" }, "geometry": { "type": "Point", "coordinates": [ 7.480000437740641, 9.070246516495853 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Kasi Cloud", "Latitude": "6.426198248401155", "Longitude": "3.4769288622425325" }, "geometry": { "type": "Point", "coordinates": [ 3.476928862242533, 6.426198248401155 ] } },
+{ "type": "Feature", "properties": { "Data Cente": "Galaxy Backbone", "Latitude": "11.997564761941852", "Longitude": "8.559067028839204" }, "geometry": { "type": "Point", "coordinates": [ 8.559067028839204, 11.997564761941852 ] } }
+]
+}
